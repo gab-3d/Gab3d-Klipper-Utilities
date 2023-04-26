330a14e23b5ba9309b438a298741ec01439086fb
Gabriele Castellani <GabrieleCastellani@users.noreply.github.com>
2023-04-26 10:30:53 +0200

    ok

--- firstrun.sh
+++ firstrun.sh
@@ -1,14 +1,3 @@
-sudo apt update
-
-sudo apt install python3-numpy python3-matplotlib libatlas-base-dev
-
-#check numpy is installed in ~/klippy-env/bin/pip if not install it
-if ~/klippy-env/bin/pip list | grep -q 'numpy'; then
-  echo "numpy is already installed in ~/klippy-env/bin/pip"
-else
-  echo "numpy is not installed in ~/klippy-env/bin/pip, installing it now"
-  ~/klippy-env/bin/pip install -v numpy
-fi
 
 
 bash ~/Gab3d-Klipper-Utilities/install.sh
--- install.sh
+++ install.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
-echo "Install.sh"
+echo "running Install.sh"
+sudo apt update
+
+sudo apt install python3-numpy python3-matplotlib libatlas-base-dev
+
+#check numpy is installed in ~/klippy-env/bin/pip if not install it
+if ~/klippy-env/bin/pip list | grep -q 'numpy'; then
+  echo "numpy is already installed in ~/klippy-env/bin/pip"
+else
+  echo "numpy is not installed in ~/klippy-env/bin/pip, installing it now"
+  ~/klippy-env/bin/pip install -v numpy
+fi
 
 
 #check if  a symbolic link to the file ~/Gab3d-Klipper-Utilities/gab-3d-utilities.cfg in /home/pi/printer_data/config/ exist if not create it
