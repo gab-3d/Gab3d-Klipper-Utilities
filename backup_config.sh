2dbfe1731af8e4a32dbec54c4449879a3fba2f90
Gabriele Castellani <GabrieleCastellani@users.noreply.github.com>
2023-04-26 14:05:11 +0200

    ok

--- backup_config.sh
+++ backup_config.sh
@@ -15,9 +15,8 @@ fi
 
 #create a variable backupdate with a string rapresenting the current date and time in minutes separated by minus
 backupdate=$(date +%Y%m%d-%H%M)
-echo $backupdate
 #zip ~/printer_data/config directory to a file named hostname-date.zip and exclude .git directory
-zip -r ~/printer_data/config/backup/$(hostname)-$backupdate.zip ~/printer_data/config -x \.git\*
+zip -r ~/printer_data/config/backup/$(hostname)-$backupdate.zip ~/printer_data/config -x *.zip 
 
 #add /etc/network/interfaces.d/can0
 zip -r ~/printer_data/config/backup/$(hostname)-$backupdate.zip /etc/network/interfaces.d/can0
