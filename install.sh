ab223aa3d6e11e53bf3d90923d215aa83d36920f
Gabriele Castellani <GabrieleCastellani@users.noreply.github.com>
2023-04-26 09:22:30 +0200

    fix

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-#check if the string 'Gab3d-Klipper-Utilities.git' exists in the file /home/pi/printer_data/config/moonracker.conf
-if grep -q 'Gab3d-Klipper-Utilities.git' /home/pi/printer_data/config/moonracker.conf; then
-  echo "Gab3d-Klipper-Utilities.git already exists in /home/pi/printer_data/config/moonracker.conf"
+#check if the string 'Gab3d-Klipper-Utilities.git' exists in the file /home/pi/printer_data/config/moonraker.conf
+if grep -q 'Gab3d-Klipper-Utilities.git' /home/pi/printer_data/config/moonraker.conf; then
+  echo "Gab3d-Klipper-Utilities.git already exists in /home/pi/printer_data/config/moonraker.conf"
 else
-  #append the string 'Gab3d-Klipper-Utilities.git' to the file /home/pi/printer_data/config/moonracker.conf
-    echo "Gab3d-Klipper-Utilities.git does not exist in /home/pi/printer_data/config/moonracker.conf, adding it now"
+  #append the string 'Gab3d-Klipper-Utilities.git' to the file /home/pi/printer_data/config/moonraker.conf
+    echo "Gab3d-Klipper-Utilities.git does not exist in /home/pi/printer_data/config/moonraker.conf, adding it now"
     echo  "
 
     [update_manager Gab3d-Klipper-Utilities]
@@ -14,7 +14,7 @@ else
     path: ~/Gab3d-Klipper-Utilities
     origin: https://github.com/gab-3d/Gab3d-Klipper-Utilities.git
     install_script: install.sh
-    " >> /home/pi/printer_data/config/moonracker.conf
+    " >> /home/pi/printer_data/config/moonraker.conf
 fi
 
   
