8a392b2b528c078fcf7db1d90091852f851b3334
Gabriele Castellani <GabrieleCastellani@users.noreply.github.com>
2023-04-26 13:36:36 +0200

    fix

--- README.md
+++ README.md
@@ -1,10 +1,12 @@
 # Gab3d Klipper Utilities
  
 To install it open a shell prompt (ssh or putty)
-run
+run these 3 lines
+
 ```shell
 cd ~
-git clone https://github.com/gab-3d/Gab3d-Klipper-Utilities.git && bash ~/Gab3d-Klipper-Utilities/install.sh
+git clone https://github.com/gab-3d/Gab3d-Klipper-Utilities.git
+bash ~/Gab3d-Klipper-Utilities/install.sh
 ```
 
 # Utilities included
--- backup_config.sh
+++ backup_config.sh
@@ -16,8 +16,11 @@ fi
 #create a variable backupdate with a string rapresenting the current date and time in minutes separated by minus
 backupdate=$(date +%Y%m%d-%H%M)
 echo $backupdate
-#zip ~/printer_data/config directory to a file named hostname-date.zip
-zip -r ~/printer_data/config/backup/$(hostname)-$backupdate.zip ~/printer_data/config
+#zip ~/printer_data/config directory to a file named hostname-date.zip and exclude .git directory
+zip -r ~/printer_data/config/backup/$(hostname)-$backupdate.zip ~/printer_data/config -x \.git\*
 
 #add /etc/network/interfaces.d/can0
 zip -r ~/printer_data/config/backup/$(hostname)-$backupdate.zip /etc/network/interfaces.d/can0
+
+zip -r ~/printer_data/config/backup/$(hostname)-$backupdate.zip ~/printer_data/moonraker.asvc
+
