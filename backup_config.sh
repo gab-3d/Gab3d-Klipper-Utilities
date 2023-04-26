4189177de2888ea70fd5afcf20c8fef365225ac4
Gabriele Castellani <GabrieleCastellani@users.noreply.github.com>
2023-04-26 11:31:52 +0200

    reedit

--- backup_config.sh
+++ backup_config.sh
@@ -1,27 +1,23 @@
 #!/bin/bash
-# This script is used to backup a user's /home/pi/printer_data/config directory to /home/pi/printer_data/config/backup
+# This script is used to backup a user's ~/printer_data/config directory to ~/printer_data/config/backup
 # It also backup can0 configuration
-# It also backup /home/pi/printer_data/gcode directory to /home/pi/printer_data/config/backup
+# It also backup ~/printer_data/gcode directory to ~/printer_data/config/backup
 
 
-#check if /home/pi/printer_data/config/backup exists if not create it
-if [ ! -d /home/pi/printer_data/config/backup ]; then
+#check if ~/printer_data/config/backup exists if not create it
+if [ ! -d ~/printer_data/config/backup ]; then
   echo "backup directory does not exist, creating it now"
-  mkdir /home/pi/printer_data/config/backup
+  mkdir ~/printer_data/config/backup
 fi
 
-#check if zip is installed if not install it
-if [ ! -f /usr/bin/zip ]; then
-  echo "zip is not installed, installing it now"
-  sudo apt-get install zip -y
-fi
+
 
 
 #create a variable backupdate with a string rapresenting the current date and time in minutes separated by minus
 backupdate=$(date +%Y%m%d-%H%M)
 echo $backupdate
-#zip /home/pi/printer_data/config directory to a file named hostname-date.zip
-zip -r /home/pi/printer_data/config/backup/$(hostname)-$backupdate.zip /home/pi/printer_data/config
+#zip ~/printer_data/config directory to a file named hostname-date.zip
+zip -r ~/printer_data/config/backup/$(hostname)-$backupdate.zip ~/printer_data/config
 
 #add /etc/network/interfaces.d/can0
-zip -r /home/pi/printer_data/config/backup/$(hostname)-$backupdate.zip /etc/network/interfaces.d/can0
+zip -r ~/printer_data/config/backup/$(hostname)-$backupdate.zip /etc/network/interfaces.d/can0
